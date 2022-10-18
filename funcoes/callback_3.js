f20cfca37abe8486a8d9cb234e6a08fca6b207bc
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-10-18 09:23:53 -0300

    Curso JS funcional e reativo - callback_3 (Função Map)

---
+++ funcoes/callback_3.js
@@ -0,0 +1,20 @@
+// Função .map => recebe uma função como parâmetro e retorna um array 
+const nums = [1, 2, 3, 4, 5];
+const dobro = (n, i, a) => n * 2 + i + a.length;
+
+console.log(nums.map(dobro));
+
+// Map não altera valores do array ele criar uma novo array
+const nomes = ['Ana', 'Bia', 'Gui', 'Lia', 'Rafa'];
+const primeraLetra = texto => texto[0];
+const letras = nomes.map(primeraLetra);
+
+console.log(nomes, letras);
+
+const carrinho = [
+  { nome: 'Canete', qtde: 10, preco: 7.99 },
+  { nome: 'Impressora', qtde: 0, preco: 649.50 },
+  { nome: 'Caderno', qtde: 4, preco: 27.10 },
+  { nome: 'Lapis', qtde: 3, preco: 5.82 },
+  { nome: 'Tesoura', qtde: 1, preco: 19.20 },
+];
