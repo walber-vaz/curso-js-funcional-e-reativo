a0aa27f86eefe331362abad0930c5a28a5563777
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-10-20 10:36:29 -0300

    Curso JS funcional e reativo - Refatorando exercicio de funções como parâmetro

--- desafios/funcoes/desafio_2.js
+++ desafios/funcoes/desafio_2.js
@@ -1,34 +1,15 @@
 // somar(3)(4)(5)
-function somar(num1) {
-  return function(num2) {
-    return function(num3) {
-      return num1 + num2 + num3
-    }
-  }
-}
+const somar = (num1) => (num2) => (num3) => num1 + num2 + num3
 console.log(somar(3)(4)(5))
 
 // calcular(3)(7)(fn)
-function calcular(num1) {
-  return function(num2) {
-    return function(fn) {
-      if (typeof fn === 'function') {
-        return fn(num1, num2)
-      }
-    }
+const calcular = (num1) => (num2) => (fn) => {
+  if (typeof fn === 'function') {
+    return fn(num1, num2)
   }
 }
 
-function som(num1, num2) {
-  return num1 + num2
-}
-
-function mult(num1, num2) {
-  return num1 * num2
-}
-
-function div(num1, num2) {
-  return num1 / num2
-}
-
+// const som = (num1, num2) => num1 + num2
+const mult = (num1, num2) => num1 * num2
+// const div = (num1, num2) => num1 / num2
 console.log(calcular(3)(7)(mult))
