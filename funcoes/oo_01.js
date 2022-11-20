3d6257783415d05a208f6cac9f7377a697e701b2
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-11-20 19:49:44 -0300

    Curso JS funcional e reativo - Principio do OOP (this)

---
+++ funcoes/oo_01.js
@@ -0,0 +1,16 @@
+function Produto(name, price, desc = 0.15) {
+  this.name = name
+  this.price = price
+  this.desc = desc
+
+  this.priceFinal = function () {
+    return this.price * (1 - this.desc)
+  }
+}
+
+const p1 = new Produto('Caneta', 1.5)
+console.log(p1.name)
+
+const p2 = new Produto('Notebook', 11.5)
+console.log(p2.price)
+console.log(p2.priceFinal())
