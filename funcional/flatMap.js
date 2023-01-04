cd90be1e719feef3667c6078ded4912f8c427bab
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-01-04 15:00:27 -0300

    Funcional Conceito: FlatMap

---
+++ funcional/flatMap.js
@@ -0,0 +1,7 @@
+const letras = [['o', 'l', 'á'], [' '], ['m', 'u', 'n', 'd', 'o', '!']]
+
+// const result = letras
+//   .map((letra) => letra.toUpperCase())
+//   .reduce((a, b) => a + b)
+
+console.log(letras.flat(Infinity))
